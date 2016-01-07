0d2469ee9a87c11d7f2ed5c895e0eaf248d37b01
user <rik.vanberkum@wur.nl>
2016-01-07 14:11:04 +0100

    add leap test

--- R/leapyear.R
+++ R/leapyear.R
@@ -1,9 +1,21 @@
 # Januari 2015
 # Saxoriko
-isleapyear <- function(x){
-	if (!is.numeric(x)){
+isleapyear <- function(year){
+	# check if its a number
+	if (!is.numeric(year)){
 		stop("not a number")
-		
+	}
+	
+	if (year < 1600 | year > 9999){
+		stop("year is out of range")
+	}
+	
+	# calculate if it is a leapyear
+	if (year %% 4 == 0) {
+		return (TRUE)
+	}
+	else {
+		return (FALSE)
 	}
 	
 }
--- main.R
+++ main.R
@@ -1,3 +1,6 @@
 source("R/leapyear.R")
 
 isleapyear("20d")
+isleapyear(1500)
+isleapyear(2002)
+isleapyear(2000)
