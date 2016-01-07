1d2a1c57b5a52127977f27f65449d3924b252ed8
user <rik.vanberkum@wur.nl>
2016-01-07 13:13:55 +0100

    dasfaf

---
+++ R/expected_outcomes.R
@@ -0,0 +1,11 @@
+is.leap(2000)
+[1] TRUE
+
+is.leap(1581)
+[1] "1581 is out of the valid range"
+
+is.leap(2002)
+[1] FALSE
+
+is.leap('john') #should return an error 
+#Error: argument of class numeric expected
